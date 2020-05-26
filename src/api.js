bcec9fa48425def1834c7ad368c3d706e703d99a
andresperlo <andres_perlo@hotmail.com>
2020-05-26 15:42:01 -0300

    se crea src/api

--- src/api.js
+++ src/api.js
@@ -1,32 +1,4 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const moongoose = require('mongoose')
-const app = express()
-app.use(morgan('dev'))
-app.use(express.json())
-/* ----------------------------- */
-const serverless = require('serverless-http')
+const serverless = require('serverless-http');
+const app = require('../backend-submodule/src/app').createApp('/.netlify/functions');
 
-//se tre el router
-const v1Routes = require('../routes')
-
-app.use('/.netlify/functions/api/v1', v1Routes)
-
-//Error 404.-
-app.use(function (req, res, next) {
-    res.status(404).json({ mensaje: 'No Funca' })
-})
-
-moongoose.connect(
-    process.env.mongodb, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}, (err) => {
-    if (err) {
-        return console.log(err)
-    }
-})
-
-module.exports.handler = serverless(app)
+exports.handler = serverless(app)
